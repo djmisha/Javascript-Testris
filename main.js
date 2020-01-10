071f105442e9b8ff3b335136cae5a0833a94e724
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2020-01-09 16:52:12 -0800

    peice on board

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ ctx.canvas.height = ROWS * BLOCK_SIZE;
 // Scale Blocks
 
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+
+let board = new Board();
+
+function play() {
+	board.getEmptyBoard();
+	let piece = new Piece(ctx);
+	piece.draw();
+
+	board.piece = piece;
+}
